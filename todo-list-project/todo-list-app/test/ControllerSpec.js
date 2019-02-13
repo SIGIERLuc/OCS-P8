e188fb65989a89aa6a4d8975b9b193b5d395359c
SIGIERLuc <lucsigier@gmail.com>
2019-02-13 18:10:21 +0100

    Update ControllerSpec.js

--- todo-list-project/todo-list-app/test/ControllerSpec.js
+++ todo-list-project/todo-list-app/test/ControllerSpec.js
@@ -61,11 +61,7 @@ describe('controller', function () {
 
 	it('should show entries on start-up', function () {
 		// TODO: write test
-		/* var todo = { title: 'my todo' };
-			setUpModel([todo]); */
-		subject.setView('');
-
-		expect(view.render).toHaveBeenCalled();
+		
 	});
 
 	describe('routing', function () {
@@ -90,22 +86,12 @@ describe('controller', function () {
 
 		it('should show active entries', function () {
 			// TODO: write test
-			var todo = { title: 'my todo', completed: false };
-			setUpModel([todo]);
-
-			subject.setView('');
-
-			expect(view.render).toHaveBeenCalledWith('showEntries', [todo]);
+			
 		});
 
 		it('should show completed entries', function () {
 			// TODO: write test
-			var todo = { title: 'my todo', completed: true };
-			setUpModel([todo]);
-
-			subject.setView('');
-
-			expect(view.render).toHaveBeenCalledWith('showEntries', [todo]);
+			
 		});
 	});
 
@@ -173,13 +159,7 @@ describe('controller', function () {
 	describe('new todo', function () {
 		it('should add a new todo to the model', function () {
 			// TODO: write test
-			var todo = { title: 'a new todo', completed: false };
-			setUpModel([todo]);
-
-			subject.setView('');
-			view.trigger('newTodo', 'a new todo');
-
-			expect(model.create).toHaveBeenCalledWith('a new todo', jasmine.any(Function));
+			
 		});
 
 		it('should add a new todo to the view', function () {
@@ -220,13 +200,7 @@ describe('controller', function () {
 	describe('element removal', function () {
 		it('should remove an entry from the model', function () {
 			// TODO: write test
-			var todo = { id: 42, title: 'my todo', completed: false };
-			setUpModel([todo]);
-
-			subject.setView('');
-			view.trigger('itemRemove', { id: 42 });
-
-			expect(model.remove).toHaveBeenCalledWith(42, jasmine.any(Function));
+			
 		});
 
 		it('should remove an entry from the view', function () {

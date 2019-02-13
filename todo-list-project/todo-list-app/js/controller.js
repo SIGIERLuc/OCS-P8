8aa721e8a8f2793dc5f6682e2b7140a4c86f4983
SIGIERLuc <lucsigier@gmail.com>
2019-02-13 18:09:19 +0100

    Revert "First Try to Jasmine test"
    This reverts commit f4ba1394dfedc71bf30a40601cd34920c6349312.

--- todo-list-project/todo-list-app/js/controller.js
+++ todo-list-project/todo-list-app/js/controller.js
@@ -72,14 +72,10 @@
 	 * Renders all active tasks
 	 */
 	Controller.prototype.showActive = function () {
-		
 		var self = this;
 		self.model.read({ completed: false }, function (data) {
-			console.log(data)
 			self.view.render('showEntries', data);
 		});
-		
-		
 	};
 
 	/**
